47f5ff4b7b685f5245b553266987e75456eed04b
tarepan <tarepan5884@gmail.com>
2021-11-30 09:52:05 +0000

    Add Act100TKYM corpus preset

--- speechcorpusy/presets/__init__.py
+++ speechcorpusy/presets/__init__.py
@@ -3,5 +3,7 @@
 from .jvs.jvs import JVS # pylint: disable=unused-import
 from .zr19.zr19 import ZR19 # pylint: disable=unused-import
 from .lj.lj import LJ # pylint: disable=unused-import
+from .act100tsukuyomi.act100tsukuyomi import Act100TKYM # pylint: disable=unused-import
 
-corpus_list = ["JVS", "ZR19", "LJ"]
+
+corpus_list = ["JVS", "ZR19", "LJ", "Act100TKYM"]
---
+++ speechcorpusy/presets/act100tsukuyomi/act100tsukuyomi.py
@@ -0,0 +1,84 @@
+"""Act100TKYM corpus handler"""
+
+
+from typing import List
+from pathlib import Path
+
+from speechcorpusy.interface import AbstractCorpus, ConfCorpus, ItemId
+from speechcorpusy.helper.adress import get_adress
+from speechcorpusy.helper.contents import get_contents
+from speechcorpusy.helper.forward import forward
+
+
+# Act100TKYM: 'voiceactress100 (sub) corpus by つくよみちゃん/Tsukuyomi-chan'
+#
+# Directory structure:
+# ```
+# é┬é¡éµé▌é┐éßé±âRü[âpâX Vol.1 É║ùDô¥îvâRü[âpâXüiJVSâRü[âpâXÅÇïÆüj/
+#     01 WAVüiÄ√ÿ^Ä₧é╠ë╣ù╩é╠é▄é▄üj/
+#         VOICEACTRESS100_001.wav
+#         ...
+#         VOICEACTRESS100_100.wav
+# ```
+
+
+class Act100TKYM(AbstractCorpus):
+    """Archive/contents handler of Act100TKYM corpus.
+
+    ItemID:
+        subcorpus: "default"
+        speaker: "default"
+        name: "NNN"
+    """
+
+    _corpus_name: str = "Act100TKYM"
+    # Version and so on
+    _variant: str = "ver1_0_0"
+    _archive_name: str = "sozai-tyc-corpus1.zip"
+    _adress_origin: str = "https://tyc.rei-yumesaki.net/files/sozai-tyc-corpus1.zip"
+
+    def __init__(self, conf: ConfCorpus) -> None:
+        """Initialization without corpus contents acquisition.
+        """
+
+        super().__init__(conf)
+        self.conf = conf
+        self._adress_archive, self._path_contents = get_adress(
+            conf.root,
+            self._corpus_name,
+            self._variant,
+            self._archive_name,
+        )
+
+    def get_contents(self) -> None:
+        """Get corpus contents into local.
+        """
+
+        get_contents(
+            self._adress_archive,
+            self._path_contents,
+            self.conf.download,
+            lambda: forward(self._adress_origin, self._adress_archive),
+        )
+
+    def get_identities(self) -> List[ItemId]:
+        """Get corpus item identities.
+
+        Returns:
+            Full item identity list.
+        """
+
+        return [ItemId("default", "default", str(i).zfill(3)) for i in range(1, 101)]
+
+    def get_item_path(self, item_id: ItemId) -> Path:
+        """Get path of the item.
+
+        Args:
+            item_id: Identity of target item.
+        Returns:
+            Path of the target item.
+        """
+        root = self._path_contents
+        d_1 = "é┬é¡éµé▌é┐éßé±âRü[âpâX Vol.1 É║ùDô¥îvâRü[âpâXüiJVSâRü[âpâXÅÇïÆüj"
+        d_2 = "01 WAVüiÄ√ÿ^Ä₧é╠ë╣ù╩é╠é▄é▄üj"
+        return root / d_1 / d_2 / f"VOICEACTRESS100_{item_id.name}.wav"
